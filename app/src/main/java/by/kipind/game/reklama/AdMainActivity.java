408dead800fafc1c512fcecf19d7cc913f71d464
ifire777 <kudziev.ip@gmail.com>
2017-01-28 01:23:47 +0300

    remoove appodel libs+ back to admob

--- app/src/main/java/by/kipind/game/reklama/AdMainActivity.java
+++ app/src/main/java/by/kipind/game/reklama/AdMainActivity.java
@@ -7,8 +7,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageView;
 
-import com.appodeal.ads.Appodeal;
-import com.appodeal.ads.InterstitialCallbacks;
+import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.InterstitialAd;
 
@@ -16,68 +15,67 @@ import by.kipind.game.olympicgames.R;
 
 public class AdMainActivity extends Activity implements OnClickListener {
 
-    private final String unLockLvlAdBlock = "ca-app-pub-3924626110211690/1734845360";
+	private final String unLockLvlAdBlock = "ca-app-pub-3924626110211690/1734845360";
 
-    private InterstitialAd mInterstitialAd;
+	private InterstitialAd mInterstitialAd;
 
-    private ImageView ivLeaderBord;
-    private int loadAdErrorCode=0;
-    private boolean showAdFlag=false;
+	private ImageView ivLeaderBord;
+	private int loadAdErrorCode = 0;
+	private boolean showAdFlag = false;
 
-    @Override
-    protected void onCreate(android.os.Bundle savedInstanceState) {
-	super.onCreate(savedInstanceState);
+	@Override
+	protected void onCreate(android.os.Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-	setContentView(R.layout.ad_main);
+		setContentView(R.layout.ad_main);
 
-	ivLeaderBord = (ImageView) findViewById(R.id.imageView1);
-	ivLeaderBord.setOnClickListener(this);
+		ivLeaderBord = (ImageView) findViewById(R.id.imageView1);
+		ivLeaderBord.setOnClickListener(this);
 
-	/*mInterstitialAd = new InterstitialAd(this);
-	mInterstitialAd.setAdUnitId(unLockLvlAdBlock);
+		mInterstitialAd = new InterstitialAd(this);
+		mInterstitialAd.setAdUnitId(unLockLvlAdBlock);
 
-	mInterstitialAd.setAdListener(new AdListener() {
-	    @Override
-	    public void onAdLoaded() {
+		mInterstitialAd.setAdListener(new AdListener() {
+			@Override
+			public void onAdLoaded() {
 
-		showRevVideo();
-	    }
+				showRevVideo();
+			}
 
-	    @Override
-	    public void onAdFailedToLoad(int errorCode) {
+			@Override
+			public void onAdFailedToLoad(int errorCode) {
 
-		if (errorCode != 2) { // if there is no ad for this user in %ad_net_work% open lvl
-		    backToGame(-777);
-		} else {
-		ivLeaderBord.setVisibility(View.VISIBLE);
-		    loadAdErrorCode = errorCode;
-		}
-	    }
+				if (errorCode != 2) { // if there is no ad for this user in %ad_net_work% open lvl
+					backToGame(-777);
+				} else {
+					ivLeaderBord.setVisibility(View.VISIBLE);
+					loadAdErrorCode = errorCode;
+				}
+			}
 
-	    @Override
-	    public void onAdLeftApplication() {
-	    }
+			@Override
+			public void onAdLeftApplication() {
+			}
 
-	    @Override
-	    public void onAdClosed() {
-		// RequestNewInterstatial();
-		backToGame(-777);
-	    }
+			@Override
+			public void onAdClosed() {
+				// RequestNewInterstatial();
+				backToGame(-777);
+			}
 
-	});
-*/
+		});
 
-        Appodeal.setInterstitialCallbacks(new InterstitialCallbacks() {
+      /*  Appodeal.setInterstitialCallbacks(new InterstitialCallbacks() {
 
             @Override
             public void onInterstitialLoaded(boolean isPrecache) {
-              /* if(!showAdFlag){
-                   showAdFlag=showRevVideo();
+              *//* if(!showAdFlag){
+              	 showAdFlag=showRevVideo();
                }
 
 
-*/
-            }
+*//*
+	        }
 
             @Override
             public void onInterstitialFailedToLoad() {
@@ -104,60 +102,60 @@ public class AdMainActivity extends Activity implements OnClickListener {
 
 
         });
+*/
+		RequestInterstatial();
 
-//RequestInterstatial();
-
-        if(!showAdFlag){
-            showAdFlag=showRevVideo();
-        }
+		if (!showAdFlag) {
+			showAdFlag = showRevVideo();
+		}
 
 
-    }
-
-    protected void backToGame(int extraBackParam) {
-	Intent resIntent = new Intent();
-	resIntent.putExtra("adShowRes", extraBackParam);
-	setResult(RESULT_OK, resIntent);
-	this.finish();
-    }
-
-    protected void RequestInterstatial() {
-	AdRequest adRequest = new AdRequest.Builder().addTestDevice("D1BD3EB7210C5E0FBE523551EA523168").build();
-
-	mInterstitialAd.loadAd(adRequest);
-    }
-
-    public boolean showRevVideo() {
-	boolean res = false;
-
-        if(Appodeal.isLoaded(Appodeal.INTERSTITIAL)){
-            Appodeal.show(this, Appodeal.INTERSTITIAL);
-            loadAdErrorCode=-777;
-            res = true;
-        }else{
-
-        }
-
-	/*if (mInterstitialAd.isLoaded()) {
-	    mInterstitialAd.show();
-
-	} else {
-	}*/
-
-	return res;
-    }
-
-    @Override
-    public void onClick(View v) {
-	if (v.equals(this.ivLeaderBord)) {
-	    backToGame(loadAdErrorCode);
 	}
 
-    }
+	protected void backToGame(int extraBackParam) {
+		Intent resIntent = new Intent();
+		resIntent.putExtra("adShowRes", extraBackParam);
+		setResult(RESULT_OK, resIntent);
+		this.finish();
+	}
 
-    @Override
-    public void onBackPressed() {
-	backToGame(loadAdErrorCode);
-    }
+	protected void RequestInterstatial() {
+		AdRequest adRequest = new AdRequest.Builder().addTestDevice("D1BD3EB7210C5E0FBE523551EA523168").build();
+
+		mInterstitialAd.loadAd(adRequest);
+	}
+
+	public boolean showRevVideo() {
+		boolean res = false;
+
+	/*	if (Appodeal.isLoaded(Appodeal.INTERSTITIAL)) {
+			Appodeal.show(this, Appodeal.INTERSTITIAL);
+			loadAdErrorCode = -777;
+			res = true;
+		} else {
+
+		}*/
+
+		if (mInterstitialAd.isLoaded()) {
+			mInterstitialAd.show();
+
+		} else {
+		}
+
+		return res;
+	}
+
+	@Override
+	public void onClick(View v) {
+		if (v.equals(this.ivLeaderBord)) {
+			backToGame(loadAdErrorCode);
+		}
+
+	}
+
+	@Override
+	public void onBackPressed() {
+		backToGame(loadAdErrorCode);
+	}
 
 }
